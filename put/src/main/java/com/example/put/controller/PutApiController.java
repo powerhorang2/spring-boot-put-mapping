2c4da5b1314582b5810a895419cc42f7e892e187
powerhorang2 <tjrdbqls3@naver.com>
2021-08-13 15:37:59 +0900

    REST API - PUT / Path Variable 추가

--- put/src/main/java/com/example/put/controller/PutApiController.java
+++ put/src/main/java/com/example/put/controller/PutApiController.java
@@ -1,5 +1,6 @@
 package com.example.put.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,12 @@ public class PutApiController {
 	@PutMapping("/put")
 	public PostRequestDto put(@RequestBody PostRequestDto requestDto) {
 		System.out.println(requestDto);
+		return requestDto;
+	}
+	
+	@PutMapping("/put/{userId}")
+	public PostRequestDto putPathVariable(@RequestBody PostRequestDto requestDto, @PathVariable(name = "userId") Long id) {
+		System.out.println(id);
 		return requestDto;
 	}
 }
